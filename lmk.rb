646da55bdae3dec6f5739324698383b7c0622a5c
Nicholas Herson <nicholas.herson@gmail.com>
2020-10-25 21:31:16 -0700

    initial commit

---
+++ lmk.rb
@@ -0,0 +1,10 @@
+class LMK < Formula
+  desc "Simple script to notify MacOS users when commands finish running"
+  homepage "https://github.com/nherson/lmk"
+  url "https://github.com/nherson/lmk/releases/download/v0.0.1/lmk.tar.gz"
+  sha256 "56586712fbbaef852c4e15c82c38c1e1ac14dbce0845ca9031e6b0fc47d9004a"
+  # license ""
+
+  depends_on "terminal-notifier"
+end
+
