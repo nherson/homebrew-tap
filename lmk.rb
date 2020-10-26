6c2626a4bd768cf0b5d4f2a57ec1e0a8f1371333
Nicholas Herson <nicholas.herson@gmail.com>
2020-10-25 21:44:02 -0700

    remove compiling

--- lmk.rb
+++ lmk.rb
@@ -4,6 +4,8 @@ class Lmk < Formula
   url "https://github.com/nherson/lmk/releases/download/v0.0.1/lmk.tar.gz"
   sha256 "56586712fbbaef852c4e15c82c38c1e1ac14dbce0845ca9031e6b0fc47d9004a"
   # license ""
+  
+  bottle :unneeded
 
   depends_on "terminal-notifier"
 end
